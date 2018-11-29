48d8e2106ffbb589ef93f85b46c91f61c6041475
Benoit Favre <benoit.favre@lis-lab.fr>
2018-11-29 22:18:16 +1000

    fix location of config

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
 git submodule init
 git submodule update
 
+ROOT=$PWD
+
 pushd toolchain/buildroot
 
-make defconfig make defconfig BR2_DEFCONFIG=../buildroot.config.rs97
+make defconfig make defconfig BR2_DEFCONFIG=$ROOT/buildroot.config.rs97
 make
 
 popd
 
-source env.sh
 pushd ports/sdl_test
+source $ROOT/env.sh
 make
 file sdl_test
 popd
